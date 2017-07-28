a8b544438d9a3ca42426dc8875340bcaa1058b66
toanvv <toanvv@zigexn.vn>
2017-07-28 08:42:25 +0700

    update crawler

--- crawler.rb
+++ crawler.rb
@@ -4,21 +4,21 @@ require 'open-uri'
 require 'csv'
 require 'timeout'
 
+# Requester
 class Requester
   include Celluloid
 
   # Request
   def request(url)
-    @st = :busy
     result = Timeout.timeout(3) { Nokogiri::HTML(open(url)) } rescue 'TIMEOUT_ERR'
-    @st = :free
     [url, result]
   end
 end
 
+# Self heal processor
 class Processor
   include Celluloid
-  
+
   def initialize
     @st = :free
   end
@@ -31,45 +31,46 @@ class Processor
     CSV.open('./db/townwork-result.csv', 'ab') { |csv| csv << [url, kw] }
     @st = :free
   end
-  
+
   def free?
     @st == :free
   end
 end
 
-class TaskManager
+# Crawler using processor to process data, requester to handler request
+class Crawler
   include Celluloid
   REQUEST_INTERVAL = 2
 
-  attr_reader :raw_html, :actors, :urls
-  
-  def initialize(actors, urls)
-    @actors = actors
+  attr_reader :raw_html, :processor, :requester, :urls
+
+  def initialize(processor, urls)
+    @processor = processor
+    @requester = requester
     @raw_html = []
     @urls = urls
   end
-  
-  def free_actor
-    actors.find { |actor| actor.free? }
+
+  def free_processor
+    processor.find { |actor| actor.free? }
   end
-  
+
   def rock!
     every(REQUEST_INTERVAL) {
-      raw = Celluloid::Actor[:requester].async.request(urls.pop)
+      raw = requester.async.request(urls.pop)
       raw_html << raw if raw.present?
     }
 
     loop do
-      # if there are no free actor
       if raw_html.blank?
-        sleep 1  # Take a rest
+        sleep 1 # Take a rest
         next if raw_html.blank?
       end
 
       raw_html.delete_if do |raw|
-        actor = free_actor
-        free_actor.async.process(raw) if actor.present?
-        actor.present?
+        processor = free_processor
+        processor.async.process(raw) if processor.present?
+        processor.present?
       end
     end
   end
@@ -78,16 +79,18 @@ end
 # get list of URLS
 urls = CSV.read('./db/sitemap.csv')
 
-
 # Create 10 proxy crawlers
 container = Celluloid::Supervision::Container.new do
-  10.times { |i| supervise type: Processor, as: "processor_#{i}" }
-  # Or shorter
+  names = (1..10).map { |name| "processor_#{name}" }
+  names.each { |name| Celluloid.supervise type: Processor, as: "processor_#{name}" }
 end
 
-manager = TaskManager.new()
-
-Celluloid.supervise type: Requester, as: :requester
+processors = names.map { |name| Celluloid::Actor[name] }
 
 # Supervise those proxy crawlers
 container.async.run
+
+Celluloid.supervise type: Requester, as: :requester
+requester = Celluloid::Actor[:requester]
+
+Crawler.new(processors, requester, urls).rock!
